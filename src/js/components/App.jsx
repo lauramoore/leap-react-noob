f30a0f4adc518ed0b941e5973b0332c6f1b76dc4
lauramoore <laura.moore@rightbox.com>
2015-05-25 11:55:39 -0400

    control jumbotron class from leapmotion pitch

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,29 +2,60 @@ import React, {PropTypes} from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import LeapState from './LeapStatus.jsx';
+import HandStore from '../stores/HandStore';
+
+function colorForPitch(hand){
+  if (! hand) return 'grey';
+  var pitchVal = hand['pitch'];
+  if (pitchVal > 0) {
+    return 'blue';
+  } if (pitchVal < 0) {
+     return 'red';
+  } else {
+     return 'grey';
+  }
+}
 
 export default React.createClass({
-  propTypes: {
-    hand: PropTypes.any.isRequired
+   _onChange() {
+    var newState = HandStore.getPitchYawRoll();
+    this.setState({hand: newState});
   },
 
-  getDefaultProps() {
+  getInitialState() {
+     return HandStore.getPitchYawRoll()
+  },
+
+  componentDidMount() {
+    HandStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    HandStore.removeChangeListener(this._onChange);
+  },
+
+  getDefaultState() {
     return {
-      hand: false
+       hand: {}
     }
   },
 
   render() {
-    let {hand} = this.props;
+    var hand = this.state.hand;
     var jumbotronMessage;
     if (hand) {
        jumbotronMessage = <p>TODO Show Hand Info</p>
     } else {
        jumbotronMessage = <p>Put Hand Over Leap Motion</p>
     }
+    
+    var styles = {
+       backgroundColor : colorForPitch(hand)
+    }
+    
     return (
       <div className="container">
-        <Jumbotron>
+        <Jumbotron style={styles}>
           <h1>LeapMotion</h1>
           {jumbotronMessage}
         </Jumbotron>
--- src/js/components/AppContainer.jsx
+++ src/js/components/AppContainer.jsx
@@ -1,34 +1,11 @@
 import React from 'react';
-import TodoStore from '../stores/TodoStore';
-import HandStore from '../stores/HandStore';
-import ActionCreator from '../actions/TodoActionCreators';
 import App from './App.jsx';
 
 export default React.createClass({
-  _onChange() {
-    this.setState(HandStore.getPitchYawRoll());
-    console.log(this.state);
-  },
-
-  getInitialState() {
-     return HandStore.getPitchYawRoll()
-  },
-
-  componentDidMount() {
-    HandStore.addChangeListener(this._onChange);
-  },
-
-  componentWillUnmount() {
-    HandStore.removeChangeListener(this._onChange);
-  },
-
-
   
   render() {
-    let {hand} = this.state;
     return (
-      <App
-        hand={hand} />
+      <App/>
     );
   }
 });
