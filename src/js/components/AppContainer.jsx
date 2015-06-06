9366e780be564f419c4dd69bf967e1496abf4d6f
lauramoore <laura.moore@rightbox.com>
2015-06-06 16:25:51 -0400

    migrated toggle behavior out of App to a component

--- src/js/components/AppContainer.jsx
+++ src/js/components/AppContainer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import App from './App.jsx';
+import PitchTogglePanel from './PitchTogglePanel.jsx';
 
 export default React.createClass({
   
   render() {
     return (
-      <App/>
+      <PitchTogglePanel/>
     );
   }
 });
---
+++ src/js/components/PitchTogglePanel.jsx
@@ -0,0 +1,74 @@
+import React, {PropTypes} from 'react';
+import Button from 'react-bootstrap/lib/Button';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import LeapState from './LeapStatus.jsx';
+import HandStore from '../stores/HandStore';
+
+function toggleColorByPitch(hand){
+  var noHandColor = 'grey';
+  var upColor = 'red';
+  var downColor = 'blue';
+ 
+  function resolveBackgroundFromPitch(){
+    if (! hand) return noHandColor;
+      var pitchVal = hand['pitch'];
+      if (pitchVal > 0) {
+         return upColor;
+      } if (pitchVal < 0) {
+         return downColor;
+      } else {
+         return noHandColor;
+      }
+  }
+  return {
+     backgroundColor :  resolveBackgroundFromPitch()
+  };
+  
+}
+
+export default React.createClass({
+   _onChange() {
+    var newState = HandStore.getPitchYawRoll();
+    this.setState({hand: newState});
+  },
+
+  getInitialState() {
+     return HandStore.getPitchYawRoll()
+  },
+
+  componentDidMount() {
+    HandStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    HandStore.removeChangeListener(this._onChange);
+  },
+
+  getDefaultState() {
+    return {
+       hand: {}
+    }
+  },
+
+  render() {
+    var hand = this.state.hand;
+    var jumbotronMessage;
+    if (hand) {
+       jumbotronMessage = <p>TODO Show Hand Info</p>
+    } else {
+       jumbotronMessage = <p>Put Hand Over Leap Motion</p>
+    }
+    
+    var styles = toggleColorByPitch(this.state.hand);
+    
+    return (
+      <div className="container">
+        <Jumbotron style={styles}>
+          <h1>LeapMotion</h1>
+          {jumbotronMessage}
+        </Jumbotron>
+        <LeapState />
+      </div>
+    );
+  }
+});
