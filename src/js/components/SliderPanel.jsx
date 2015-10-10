56c8b2eea4bdb7ab418159ae5d1618bea2b248b0
lauramoore <laura.moore@rightbox.com>
2015-10-10 15:46:27 -0400

    slider is fully hand driven

--- src/js/components/SliderPanel.jsx
+++ src/js/components/SliderPanel.jsx
@@ -3,23 +3,24 @@ import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import LeapMotionStore from '../stores/LeapMotionStore';
 
 function computeGradient(distance){
+   console.log(distance);
    var length = 100;
    var  outside = (length - distance) / 2;
    var a = outside;
    var b = outside + distance;
    return {
     background: 'linear-gradient(to right, ' 
-                + 'rgba(239,195,206,1) 0%, ' 
-                + 'rgba(185,39,76,1) '+ a +'%, '
-                + 'rgba(168,20,60,1) ' + b + '%, '
-                +'rgba(241,142,166,1) 100%)'
+                + 'rgba(37,12,196,1) 0%, ' 
+                + 'rgba(248,240,32,1) '+ a +'%, '
+                + 'rgba(248,240,32,1) ' + b + '%, '
+                +'rgba(37,12,196,1) 100%)'
    };
 }
 export default React.createClass({
    _onChange() {
     var handList = LeapMotionStore.getHands();
     if (! handList || handList.length < 2) return;
-    var handSpread = Math.abs(handList[0].centerPoint[0] - handList[1].centerPoint[0]);
+    var handSpread = Math.abs(handList[0].centerPoint[0] - handList[1].centerPoint[0]) * 100;
     this.setState({
       styles : computeGradient(handSpread)
     });
@@ -35,7 +36,7 @@ export default React.createClass({
 
   getInitialState(){
     return {
-       styles : { background: 'blue' }
+       styles : { background: 'white' }
     }
   },
 
